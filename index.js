be077c84a4ac6352090f4d8642d463ba5831f915
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-10-28 20:52:21 -0400

    feat: setup cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express =  require('express')
 const app = express()
 
+var cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(3000, function(){
 console.log("Server rodando")
