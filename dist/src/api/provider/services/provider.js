08fe0fb24791732a01fff8a4f588be078f48057f
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-18 10:49:09 -0400

    feat: create provider

---
+++ dist/src/api/provider/services/provider.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * provider service.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::provider.provider');
